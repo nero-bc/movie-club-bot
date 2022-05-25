6a6fe3c472fe48237c10b3244945d6819fc1b337
Helena Rasche <hxr@hx42.org>
2022-05-25 18:32:12 +0200

    blah

--- web/admin.py
+++ web/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import MovieSuggestion
 
-admin.site.register(MovieSuggestion)
+class MovieSuggestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'rating', 'watched', 'suggested_by', 'get_score')
+    list_filter = ('year', 'watched', 'suggested_by')
+
+admin.site.register(MovieSuggestion, MovieSuggestionAdmin)
