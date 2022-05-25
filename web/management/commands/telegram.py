c18305c5e70eb0b74f1ab75ec71841bec68e323d
Helena Rasche <hxr@hx42.org>
2022-05-25 17:22:51 +0200

    add movies to our database

--- web/management/commands/telegram.py
+++ web/management/commands/telegram.py
@@ -1,19 +1,30 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db.utils import ProgrammingError
-import json
-import datetime
+
 from web.models import MovieSuggestion
+
+import isodate
+from bs4 import BeautifulSoup
+import datetime
+import json
+import os
+import re
 import requests
 import telebot
-import re
 import time
-import os
 
 bot = telebot.TeleBot(os.environ['TELOXIDE_TOKEN'])
 imdb_link = re.compile("imdb.com/title/(tt[0-9]*)/?")
 
 class Command(BaseCommand):
     help = "(Long Running) Telegram Bot"
+
+
+    def get_ld_json(self, url: str) -> dict:
+        parser = "html.parser"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, parser)
+        return json.loads("".join(soup.find("script", {"type":"application/ld+json"}).contents))
 
     def handle(self, *args, **options):
         def handle_messages(messages):
@@ -27,6 +38,10 @@ class Command(BaseCommand):
                 elif message.text.startswith('/countdown'):
                     if len(message.text.split()) == 2:
                         length = int(message.text.split()[1])
+                        if length > 10:
+                            length = 10
+                        elif length < 1:
+                            length = 1
                     else:
                         length = 5
 
@@ -41,11 +56,19 @@ class Command(BaseCommand):
                             bot.send_message(message.chat.id, f"{m} known.")
                         except MovieSuggestion.DoesNotExist:
                             print("New movie! Adding it to the database")
+                            movie_details = self.get_ld_json(f"https://www.imdb.com/title/{m}/")
                             movie = MovieSuggestion.objects.create(
                                 imdb_id=m,
+                                title=movie_details['name'],
+                                year=int(movie_details['datePublished'].split('-')[0]),
+                                rating=movie_details['aggregateRating']['ratingValue'],
+                                ratings=movie_details['aggregateRating']['ratingCount'],
+                                runtime=isodate.parse_duration(movie_details['duration']).seconds / 60,
                             )
                             movie.save()
                             bot.send_message(message.chat.id, f"{m} looks like a new movie, added it to the database.")
+
+                            time.sleep(1)
 
         bot.set_update_listener(handle_messages)
         bot.infinity_polling()
