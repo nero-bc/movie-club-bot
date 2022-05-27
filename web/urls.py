426f0eee7c172ac1f7c90de1a28381769fc686fc
Helena Rasche <hxr@hx42.org>
2022-05-27 17:51:39 +0200

    Add profile pages

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('u/<acct>', views.profile, name='profile'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+import collections
 from .models import MovieSuggestion
 from django.template import loader
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -10,5 +12,37 @@ def index(request):
     context = {
         'unwatched': sorted(MovieSuggestion.objects.filter(watched=False), key=lambda x: -x.get_score),
         'watched': MovieSuggestion.objects.filter(watched=True).order_by('-watched_date')
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def profile(request, acct):
+    template = loader.get_template('profile.html')
+    u = User.objects.get(username=acct)
+    suggested = u.moviesuggestion_set.all().order_by('-added')
+
+    genres = collections.Counter()
+
+    for s in suggested:
+        g = s.genre
+        if g is None:
+            continue
+
+        if g.startswith('['):
+            genre_list = eval(g)
+            # TODO: remove later.
+            s.genre = ','.join(genre_list)
+            s.save()
+        else:
+            genre_list = g.split(',')
+
+        for genre in genre_list:
+            genres[genre] += 1
+
+    context = {
+        'acct': u,
+        'suggested': suggested,
+        'genres': list(genres.most_common(5)),
+        'ratings': u.criticrating_set.all(),
     }
     return HttpResponse(template.render(context, request))
