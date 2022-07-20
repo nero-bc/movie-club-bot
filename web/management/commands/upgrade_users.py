95d3f673002009789dc08092256ad3c065f11207
Helena Rasche <hxr@hx42.org>
2022-07-20 20:07:45 +0200

    add better upgrader

--- web/management/commands/upgrade_users.py
+++ web/management/commands/upgrade_users.py
@@ -1,15 +1,20 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Permission
+import re
+import web.models
 
 
 class Command(BaseCommand):
     help = "Update existing user permissions"
 
     def handle(self, *args, **options):
+        tables = [x for x in dir(web.models) if x[0] == x[0].upper() and x[0] != '_' and x != 'User']
+        fixed = [re.sub(r'([A-Z])', r' \1', x[0].lower() + x[1:]).lower() for x in tables]
+
         for user in User.objects.all():
             for permission in ('view', 'add', 'change'):
-                for table in ('movie suggestion', 'buff', 'critic rating'):
+                for table in fixed:
                     user.user_permissions.add(Permission.objects.get(name=f"Can {permission} {table}"))
 
             user.save()
