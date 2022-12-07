5cf2b591c0beca2fe9fe1b3437bb9973127f381b
Helena Rasche <hexylena@galaxians.org>
2022-12-07 21:32:19 +0100

    filtering

--- web/management/commands/telegram.py
+++ web/management/commands/telegram.py
@@ -305,7 +305,12 @@ class Command(BaseCommand):
 
     def send_removal_poll(self, message):
         question = 'Pick one of these to DELETE from our watchlist.'
-        options = sorted(MovieSuggestion.objects.all(), key=lambda x: x.get_score)[0:2]
+        # Only unwatched
+        asdf = MovieSuggestion.objects.filter(status=0)
+        # Only movies over 100 days and unwatched are game
+        asdf = [x for x in asdf if x.days_since_added > 100]
+        # Get the worst TWO.
+        options = sorted(asdf, key=lambda x: x.get_score)[0:2]
 
         option_text = [f"{x.title} ({x.year}), added {x.days_since_added} days ago, rating {x.rating}" for x in options]
         option_nums = [str(x.imdb_id) for x in options]
